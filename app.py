87754c85feea1abc192a0494af26fdf3609d12ad
Mascotte1208 <LUCASMAIGRET1208@outlook.fr>
2026-09-04 15:58:06 -0600

    Delete app.py

--- app.py
+++
@@ -1,4 +0,0 @@
-File "/mount/src/mon-assistant-personel/app.py", line 1083
-                      st.info(fAffichage du graphique indisponible : {e})
-                              ^
-SyntaxError: invalid syntax. Perhaps you forgot a comma?
