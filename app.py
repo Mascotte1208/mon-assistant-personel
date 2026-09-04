03d23bd604332ed85e2336b496b03da2e81ed695
Mascotte1208 <LUCASMAIGRET1208@outlook.fr>
2026-09-04 15:56:03 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,4 @@
+File "/mount/src/mon-assistant-personel/app.py", line 1083
+                      st.info(fAffichage du graphique indisponible : {e})
+                              ^
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
